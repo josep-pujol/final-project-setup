af97f9c1eacd85975086e547e378a629d058722f
Ubuntu <ubuntu@ip-172-31-37-114.ec2.internal>
2019-11-05 16:51:32 +0000

    created simple model and django admin site

--- dummyApp/admin.py
+++ dummyApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserInfo, Department
+
+admin.site.register(UserInfo)
+admin.site.register(Department)
--- dummyApp/models.py
+++ dummyApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class UserInfo(models.Model):
+
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email_address = models.EmailField(max_length=254)
+    notes = models.TextField()
+    registered = models.DateTimeField('date registered')
+
+    def __str__(self):
+        return self.email_address
+
+
+class Department(models.Model):
+    user = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+
+
+
+# class Task(models.Model):
+#     user = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=200)
+#     category = models.CharField
+#     votes = models.IntegerField(default=0)
