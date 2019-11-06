f3eafc347522de306b526185cf4c47eb1a1adf94
Ubuntu <ubuntu@ip-172-31-37-114.ec2.internal>
2019-11-06 00:22:56 +0000

    added more models

--- dummyApp/models.py
+++ dummyApp/models.py
@@ -1,20 +1,7 @@
 from django.db import models
 
 
-class UserInfo(models.Model):
-
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
-    email_address = models.EmailField(max_length=254)
-    notes = models.TextField()
-    registered = models.DateTimeField('date registered')
-
-    def __str__(self):
-        return self.email_address
-
-
 class Department(models.Model):
-    user = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     description = models.TextField()
     
@@ -22,7 +9,27 @@ class Department(models.Model):
         return self.name
 
 
+class UserInfo(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email_address = models.EmailField(max_length=254)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    notes = models.TextField()
+    # Timestamp at creation
+    registered = models.DateTimeField(auto_now_add=True, editable=False)
 
+    def __str__(self):
+        return self.email_address
+
+
+class Email(models.Model):
+    users = models.ManyToManyField(UserInfo)
+    number_emails = models.PositiveIntegerField()
+    sent = models.DateTimeField()
+    
+    def __str__(self):
+        return self.users, self.number_emails
+    
 # class Task(models.Model):
 #     user = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
 #     name = models.CharField(max_length=200)
